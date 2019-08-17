e3292f1246976b1ef02eb38b8130ae385cbdb915
Ian Ibbotson <ianibbo@gmail.com>
2019-08-17 10:08:17 +0100

    Works fine with explicit transaction in controller

--- sptt/grails-app/controllers/sptt/WidgetController.groovy
+++ sptt/grails-app/controllers/sptt/WidgetController.groovy
@@ -5,17 +5,19 @@ import grails.converters.*
 import grails.gorm.multitenancy.CurrentTenant
 import grails.gorm.transactions.Transactional
 
-@CurrentTenant
 class WidgetController {
 
+  @CurrentTenant
   @Transactional
   def createWidget(String name) {
     def result=[status:'SURE']
 
-    
-    log.debug("createWidget(${params})");
-    Widget w = new Widget(widgetName:name).save(flush:true, failOnError:true);
-    log.debug(Widget.list());
+    Widget.withTransaction {
+      log.debug("createWidget(${params})");
+      Widget w = new Widget(widgetName:name).save(flush:true, failOnError:true);
+      log.debug("${Widget.list()}");
+    }
+
     render result as JSON
   }
 }
